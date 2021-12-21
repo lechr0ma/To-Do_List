05b96901f5683cea04d94d456b39a6a12d1a4fa5
lechr0ma <alex.xpomov@gmail.com>
2021-12-21 16:36:40 +1000

    added logic, some design

--- To-Do List/task/src/script.js
+++ To-Do List/task/src/script.js
@@ -1,59 +1,52 @@
+function loadTasks(){
+    document.getElementById(`task-list`).innerHTML = ``;
+    for ( let i = 0; i <= taskList.length - 1; i++) {
+        let li = document.createElement("li");
+        li.innerHTML = taskList[i].html;
+        if (taskList[i].isCrossed){
+            li.innerHTML = taskList[i].html.replace(`class="task"`, `class="task" style="text-decoration: line-through"`).replace(`type="checkbox"`, `type="checkbox" checked`);
+        }
+        document.getElementById(`task-list`).appendChild(li);
+    }}
 
-// function addTask() {
-//     if (document.getElementById('input-task').value) {
-//         let li = document.createElement("li");
-//         let ul = document.getElementById('task-list').childNodes.length;
-//         li.innerHTML = `<input type="checkbox" onchange="textChange(this)">
-//                     <span class="task">` + document.getElementById(`input-task`).value + `</span><button id="li` + ul + `" onclick="removeTask(this.parentNode)" class="delete-btn">delete task</button>`;
-//         document.getElementById(`task-list`).appendChild(li);
-//         //document.getElementById('task-list').insertBefore(li, document.getElementById(`task-list`).firstChild); //to first child
-//         document.getElementById(`input-task`).value = ``;
-//
-//     }
-// }
-function removeTask(node){
-    console.log(node);
-    node.parentNode.removeChild(node);
-    let taskList = JSON.parse(localStorage.getItem(`tasks`)) || [];
+let taskList = JSON.parse(localStorage.getItem(`tasks`)) || [] ;
+loadTasks();
 
+function removeTask(id){
+    for (let i = 0; i < taskList.length; i++){
+        if (taskList[i].id == id){
+            taskList.splice(i, 1);
+        }
+    }
+    localStorage.setItem(`tasks`, JSON.stringify(taskList));
+    loadTasks();
 }
-function textChange(node){
-    let span = node.parentNode.childNodes[1];
-    if (node.checked){
-        span.style.textDecoration = 'line-through'
-    } else{
-        span.removeAttribute('style') }
+
+function textChange(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (id == taskList[i].id) {
+            if (taskList[i].isCrossed){
+                taskList[i].isCrossed = 0;
+            }else {
+                taskList[i].isCrossed = 1;
+            }
+        }
+        localStorage.setItem(`tasks`, JSON.stringify(taskList));
+    }
+    loadTasks();
 }
-let n = 0;
-let taskList = [];
 function createTask() {
-
-    let task = {
-        text: ``,
-        html: ``,
-        status: ``
+    if (document.getElementById('input-task').value) {
+        let id = Date.now();
+        taskList.push({
+            html: `<input type="checkbox" onchange="textChange(this.id)" id="` + id + `"><span class="task">` +
+                document.getElementById('input-task').value + `</span><button id="` + id + `"onclick="removeTask(this.id)"
+                class="delete-btn">delete task</button>`,
+            isCrossed: 0,
+            id: id,
+        });
+        localStorage.setItem(`tasks`, JSON.stringify(taskList));
+        document.getElementById(`input-task`).value = ``;
+        loadTasks();
     }
-    let ul = document.getElementById('task-list').childNodes.length;
-    task.text = document.getElementById('input-task').value;
-    task.html = `<input type="checkbox" onchange="textChange(this)"><span class="task">` + task.text + `</span><button id="li` + ul + `" onclick="removeTask(this.parentNode)" class="delete-btn">delete task</button>`;
-    task.status = `created`;
-    //console.log(task);
-    taskList[n] = task;
-    localStorage.setItem(`task` + n, JSON.stringify(taskList));
-    n++;
-    let fromSt = JSON.parse(localStorage.getItem(`tasks`)) || [];
-    console.log(fromSt);
-    for ( i = 0; i <= fromSt.length - 1; i++){
-        if (fromSt[i].status == `created`){
-            console.log(fromSt[i].status);
-            let li = document.createElement("li");
-            li.innerHTML = fromSt[i].html;
-            document.getElementById(`task-list`).appendChild(li);
-
-        }
-
-        }
-
     }
-
-
